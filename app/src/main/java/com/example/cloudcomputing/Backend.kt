5912cff1d120091e9e48e852f5381f8a348a2cf6
daltonlee2k <daltonlee0032@gmail.com>
2022-02-27 16:58:17 -0700

    Set up cognito things

--- app/src/main/java/com/example/cloudcomputing/Backend.kt
+++ app/src/main/java/com/example/cloudcomputing/Backend.kt
@@ -4,6 +4,17 @@ import android.content.Context
 import android.util.Log
 import com.amplifyframework.AmplifyException
 import com.amplifyframework.core.Amplify
+import com.amplifyframework.auth.cognito.AWSCognitoAuthPlugin
+import com.amplifyframework.auth.result.AuthSignInResult
+import com.amplifyframework.auth.AuthChannelEventName
+import com.amplifyframework.core.InitializationStatus
+import com.amplifyframework.hub.HubEvent
+import com.amplifyframework.hub.HubChannel
+import com.amplifyframework.auth.cognito.AWSCognitoAuthSession
+import com.amplifyframework.auth.result.AuthSessionResult
+import android.app.Activity
+import com.amplifyframework.auth.AuthException
+import android.content.Intent
 
 object Backend {
 
@@ -11,11 +22,89 @@ object Backend {
 
     fun initialize(applicationContext: Context) : Backend {
         try {
+            Amplify.addPlugin(AWSCognitoAuthPlugin())
             Amplify.configure(applicationContext)
+
             Log.i(TAG, "Initialized Amplify")
         } catch (e: AmplifyException) {
             Log.e(TAG, "Could not initialize Amplify", e)
         }
+
+        Log.i(TAG, "registering hub event")
+
+// listen to auth event
+        Amplify.Hub.subscribe(HubChannel.AUTH) { hubEvent: HubEvent<*> ->
+
+            when (hubEvent.name) {
+                InitializationStatus.SUCCEEDED.toString() -> {
+                    Log.i(TAG, "Amplify successfully initialized")
+                }
+                InitializationStatus.FAILED.toString() -> {
+                    Log.i(TAG, "Amplify initialization failed")
+                }
+                else -> {
+                    when (AuthChannelEventName.valueOf(hubEvent.name)) {
+                        AuthChannelEventName.SIGNED_IN -> {
+                            updateUserData(true)
+                            Log.i(TAG, "HUB : SIGNED_IN")
+                        }
+                        AuthChannelEventName.SIGNED_OUT -> {
+                            updateUserData(false)
+                            Log.i(TAG, "HUB : SIGNED_OUT")
+                        }
+                        else -> Log.i(TAG, """HUB EVENT:${hubEvent.name}""")
+                    }
+                }
+            }
+        }
+        Log.i(TAG, "retrieving session status")
+
+// is user already authenticated (from a previous execution) ?
+        Amplify.Auth.fetchAuthSession(
+            { result ->
+                Log.i(TAG, result.toString())
+                val cognitoAuthSession = result as AWSCognitoAuthSession
+                // update UI
+                this.updateUserData(cognitoAuthSession.isSignedIn)
+                when (cognitoAuthSession.identityId.type) {
+                    AuthSessionResult.Type.SUCCESS ->  Log.i(TAG, "IdentityId: " + cognitoAuthSession.identityId.value)
+                    AuthSessionResult.Type.FAILURE -> Log.i(TAG, "IdentityId not present because: " + cognitoAuthSession.identityId.error.toString())
+                }
+            },
+            { error -> Log.i(TAG, error.toString()) }
+        )
         return this
     }
+
+    private fun updateUserData(withSignedInStatus : Boolean) {
+        UserData.setSignedIn(withSignedInStatus)
+    }
+
+    fun signOut() {
+        Log.i(TAG, "Initiate Signout Sequence")
+
+        Amplify.Auth.signOut(
+            { Log.i(TAG, "Signed out!") },
+            { error -> Log.e(TAG, error.toString()) }
+        )
+    }
+
+    fun signIn(callingActivity: Activity) {
+        Log.i(TAG, "Initiate Signin Sequence")
+
+        Amplify.Auth.signInWithWebUI(
+            callingActivity,
+            { result: AuthSignInResult ->  Log.i(TAG, result.toString()) },
+            { error: AuthException -> Log.e(TAG, error.toString()) }
+        )
+    }
+
+    fun handleWebUISignInResponse(requestCode: Int, resultCode: Int, data: Intent?) {
+        Log.d(TAG, "recieved requestCode : $requestCode and resultCode : $resultCode")
+        if (requestCode == AWSCognitoAuthPlugin.WEB_UI_SIGN_IN_ACTIVITY_CODE) {
+            Amplify.Auth.handleWebUISignInResponse(data)
+        }
+    }
+
+
 }
--- app/src/main/java/com/example/cloudcomputing/MainActivity.kt
+++ app/src/main/java/com/example/cloudcomputing/MainActivity.kt
@@ -1,11 +1,13 @@
 package com.example.cloudcomputing
 
+import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.Observer
 import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.content_main.*
 
@@ -18,6 +20,17 @@ class MainActivity : AppCompatActivity() {
 
         // prepare our List view and RecyclerView (cells)
         setupRecyclerView(item_list)
+        setupAuthButton(UserData)
+
+        UserData.isSignedIn.observe(this, Observer<Boolean> { isSignedUp->
+            Log.i(TAG, "isSignedIn changed : $isSignedUp")
+
+            if (isSignedUp) {
+                fabAuth.setImageResource(R.drawable.ic_baseline_lock_open)
+            } else {
+                fabAuth.setImageResource(R.drawable.ic_baseline_lock)
+            }
+        })
     }
 
     // recycler view is the list of cells
@@ -30,6 +43,25 @@ class MainActivity : AppCompatActivity() {
             // let's create a RecyclerViewAdapter that manages the individual cells
             recyclerView.adapter = NoteRecyclerViewAdapter(notes)
         })
+    }
+
+    private fun setupAuthButton(userData: UserData) {
+        fabAuth.setOnClickListener { view ->
+            val authButton = view as FloatingActionButton
+
+            if (userData.isSignedIn.value!!) {
+                authButton.setImageResource(R.drawable.ic_baseline_lock_open)
+                Backend.signOut()
+            } else {
+                authButton.setImageResource(R.drawable.ic_baseline_lock_open)
+                Backend.signIn(this)
+            }
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        Backend.handleWebUISignInResponse(requestCode, resultCode, data)
     }
 
     companion object {
